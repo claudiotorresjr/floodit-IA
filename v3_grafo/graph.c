8dd86dfe8349adcfbc7f903bcb7bd7a522d6192a
claudio <ctj17@inf.ufpr.br>
2022-03-29 10:35:47 -0300

    finalizando distancia grafo

--- v3_grafo/graph.c
+++ v3_grafo/graph.c
@@ -18,93 +18,6 @@ void initialize_array(int **colors, int size, int v)
     }
 }
 
-//int *verify_max_color_count(Graph *g, State *current, int t_colors)
-//{
-    // 
-
-    // int *colors = (int *)malloc((t_colors+1)*sizeof(int));
-    // initialize_array(&colors, t_colors+1, 0);
-
-    // int *eq_regions = (int *)malloc((t_colors+1)*sizeof(int));
-    // initialize_array(&eq_regions, t_colors+1, 0);
-
-    // int *regions = (int *)malloc((g->num_v)*sizeof(int));
-    // initialize_array(&regions, g->num_v, 0);
-
-    // for (int i = 0; i < current->size; ++i)
-    // {
-    //     int v = current->regions[i];
-    //     Vertice *aux = g->array[v].head->next;
-
-    //     // printf("verif o ");
-    //     while(aux != NULL)
-    //     {
-    //         // printf("%d ", aux->region);
-    //         if (!g->array[aux->region].head->visited && regions[aux->region] == 0)
-    //         {
-    //             // printf("%d ", aux->region);
-    //             // regions[aux->region] = 1;
-    //             colors[aux->color] += aux->size;
-    //         }
-            
-    //         // if (regions[aux->region] > 0)
-    //         {
-    //             eq_regions[aux->color]++;
-    //         }
-    //         aux = aux->next;
-    //     }
-    //     // printf("\n");
-    // }
-
-    // int max = -1;
-    // int c = -1;
-    // for (int i = 1; i < t_colors+1; ++i)
-    // {
-    //     // printf("%d: %d\n", i, colors[i]);
-    //     if (colors[i] > max)
-    //     {
-    //         max = colors[i];
-    //         c = i;
-    //     }
-    // }
-
-    // // printf("--------------------------\n");
-    // // for (int i = 1; i < t_colors+1; ++i)
-    // // {
-    // //     printf("%d ", colors[i]);
-    // // }
-    // // printf("\n");
-    // // for (int i = 1; i < t_colors+1; ++i)
-    // // {
-    // //     printf("%d ", eq_regions[i]);
-    // // }
-    // // printf("\n");
-
-    // //check_draw
-    // int bigger_region = 0;
-    // for (int i = 1; i < t_colors+1; ++i)
-    // {
-    //     // printf("vendo se a cor %d == %d\n", colors[i], max);
-    //     if (colors[i] == max)
-    //     {
-            
-    //         // printf("   cor %d (%d) > %d\n", eq_regions[i], i, bigger_region);
-    //         if (eq_regions[i] > bigger_region)
-    //         {
-    //             bigger_region = eq_regions[i];
-    //             c = i;
-    //         }
-    //     }
-    // } 
-    // // printf("maior == %d --------------------------\n", bigger_region);
-
-    // int *color = (int *)malloc(2*sizeof(int));
-    // color[0] = c;
-    // color[1] = max;
-
-    // return color;
-//}
-
 Vertice *create_vertice(int size, int color, int region, int pos[2])
 {
     Vertice *v = (Vertice *)malloc(sizeof(Vertice));
@@ -140,25 +53,7 @@ int has_edge(Graph *g, int a, int b)
             aux = aux->next;
         }
     }
-    // while (g->array[i].head != NULL)
-    // {
-    //     //head da lista eh igual ao vertice a
-    //     if (g->array[i].head->region == a)
-    //     {
-    //         aux = g->array[i].head->next;
-    //         while (aux != NULL)
-    //         {
-    //             //se vertice b ta na lista de a, retorna true (tem aresta)
-    //             if (aux->region == b)
-    //             {
-    //                 return 1;
-    //             }
-    //             aux = aux->next;
-    //         }
-    //         return 0;
-    //     }
-    //     ++i;
-    // }
+
     return 0;
 }
 
@@ -199,40 +94,6 @@ void add_edge(Graph *g, int a, int color_a, int size_a, int b, int color_b, int 
         aux->next = vertice;
         found = 1;
     }
-    //inserindo vertice b na lista do vertice a
-    // while (!found)
-    // {
-    //     //se a lista ta vazia, insere o a primeiro
-    //     if (!g->array[i].head)
-    //     {
-    //         g->num_v++;
-    //         vertice = create_vertice(size_a, color_a, a, pos);
-    //         g->array[i].head = vertice;
-
-    //         found = 1;
-    //     }
-
-    //     //se o a tem lista, adiciona o b nela
-    //     if (g->array[i].head->region == a)
-    //     {
-    //         g->array[i].head->size = size_a;
-    //         vertice = create_vertice(size_b, color_b, b, pos);
-    //         Vertice *aux = g->array[i].head;
-    //         while (aux->next)
-    //         {
-    //             aux = aux->next;
-    //         }
-
-    //         aux->next = vertice;
-    //         found = 1;
-    //     }
-    //     ++i;
-    // }
-
-    // if (found == 1)
-    // {
-    //     g->num_e++;
-    // }
 }
 
 Graph *create_graph(int size)
