c31bf059a496c349536b76b5f492d785dc8e1f42
ctj17 <ctj17@inf.ufpr.br>
2022-03-31 14:43:53 -0300

    script para os testes

--- v3_grafo/competicao.sh
+++ v3_grafo/competicao.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-echo "status;teste;jogadas;tempo" >> minhaversao.csv
-echo "status;teste;jogadas;tempo" >> professorversao.csv
+# echo "status;teste;jogadas;tempo" > minhaversao.csv
+# echo "status;teste;jogadas;tempo" > professorversao.csv
 
 for i in {1..20}
 do
@@ -14,12 +14,12 @@ do
 
     cat mapa_competicao.tmp mapa_result.tmp | ../verifica
 
-    jogadas=$(cat mapa_result.tmp | head -n 1)
     if [ "$?" -eq 0 ]
       then
-        echo "OK;$i;$jogadas;$elapsed" >> minhaversao.csv
+        jogadas=$(cat mapa_result.tmp | head -n 1)
+        echo "OK $i $jogadas $elapsed" >> minhaversao.csv
       else
-        echo "FAIL;$i;$jogadas;$elapsed"
+        echo "FAIL $i $jogadas $elapsed"
     fi
 
     #--------------------------------------------------------------------
@@ -30,5 +30,39 @@ do
     elapsed="$(bc <<<"$end_time-$start_time")"
 
     jogadas=$(cat mapa_result.tmp | head -n 1)
-    echo "OK;$i;$jogadas;$elapsed" >> professorversao.csv
+    echo "OK $i $jogadas $elapsed" >> professorversao.csv
 done
+
+gnuplot <<- EOF
+    set xlabel "Numero do teste"
+    set ylabel "Cores para solucionar"
+    set title "Cores que solucionam o tabuleiro 100x100 20 cores"   
+    set style data point
+    set style function line
+    set xtics 1
+
+    set style line 1 lc rgb "blue" lw 2
+    set style line 2 lc rgb "green" lw 2
+
+    set term png
+    set output "cores.png"
+    plot "minhaversao.csv" using 2:3 ls 1 title 'minhaversao' with lines, \
+    "professorversao.csv" using 2:3 ls 2 title 'professorversao' with lines
+EOF
+
+gnuplot <<- EOF
+    set xlabel "Numero do teste"
+    set ylabel "Tempo para solucionar"
+    set title "Tempo de execucao para solucionar o tabuleiro 100x100 20 cores"   
+    set style data point
+    set style function line
+    set xtics 1
+
+    set style line 1 lc rgb "blue" lw 2
+    set style line 2 lc rgb "green" lw 2
+
+    set term png
+    set output "tempo.png"
+    plot "minhaversao.csv" using 2:4 ls 1 title 'minhaversao' with lines, \
+    "professorversao.csv" using 2:4 ls 2 title 'professorversao' with lines
+EOF
